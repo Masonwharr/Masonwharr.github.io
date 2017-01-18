fb28ef9a4aff34586d824a6179662db3e88e975e
Masonwharr <mason.harr1@aol.com>
2017-01-18 11:50:47 -0600

    Delete websitelance.js

--- websitelance.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-var scroll = document.getElementById('top');
-var section = document.getElementsByClassName('infodiv');
-$(scroll).click(function(){
-$('html, body').animate({
-scrollTop: $(section).offset().top
-}, 'slow');
-});
-});
